8689df1b1b8db28b50a02235cd496edc8aec6411
Dominic German <dominicgerman@gmail.com>
2024-06-27 11:30:38 -0500

    Add readiness endpoints

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/dominicgerman/blog-aggregator
 
 go 1.22.3
 
-require github.com/joho/godotenv v1.5.1 // indirect
+require github.com/joho/godotenv v1.5.1
--- main.go
+++ main.go
@@ -13,14 +13,20 @@ func main() {
 	// by default, godotenv will look for a file named .env in the current directory
 	godotenv.Load()
 	port := os.Getenv("PORT")
-	filePathRoot := "."
+	if port == "" {
+		log.Fatal("PORT environment variable is not set")
+	}
 
 	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /v1/healthz", handlerReadiness)
+	mux.HandleFunc("GET /v1/err", handlerErr)
+
 	srv := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
 	}
 
-	log.Printf("Serving files from %s on port: %s\n", filePathRoot, port)
+	log.Printf("Serving on port: %s\n", port)
 	log.Fatal(srv.ListenAndServe())
 }
